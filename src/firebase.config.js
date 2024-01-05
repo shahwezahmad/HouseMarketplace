43bd3c4c480b8a548ba1d1bd1852acdc89d06f36
shahwez ahmad <shahwezahmad25@gmail.com>
2024-01-05 21:50:43 +0530

    add firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD06uAUNBhjygp6OwYGoKiRm2PwjJHzYG8",
+  authDomain: "land-market-72408.firebaseapp.com",
+  projectId: "land-market-72408",
+  storageBucket: "land-market-72408.appspot.com",
+  messagingSenderId: "11993002038",
+  appId: "1:11993002038:web:5799874a2dc41a25232897",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
